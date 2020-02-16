9a83c6c4ecfdcb658b27bd70063c2bd0f96e3d07
Gabriela Lopes <gabrieladslopes@gmail.com>
2020-02-16 18:22:03 -0300

    Added routes to create a new project and lists all projects

--- desafio01/index.js
+++ desafio01/index.js
@@ -1,8 +1,39 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ message: "hello world 1" });
+app.use(express.json());
+
+var projects = [];
+
+// Routes
+app.post('/projects', (req, res) => {
+  const { id, title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  }
+
+  projects.push(project);
+  
+  return res.json(project);
+});
+
+app.get('/projects', (req, res) => {
+  return res.json(projects);
+});
+
+app.put('/projects/:id', (req, res) => {
+
+});
+
+app.delete('/projects/:id', (req, res) => {
+
+});
+
+app.post('/projects/:id/tasks', (req, res) => {
+
 });
 
 app.listen(3333);
