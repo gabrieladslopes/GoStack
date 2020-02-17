6824618ebbe7a45671e36eb0971440f927b91e5d
Gabriela Lopes <gabrieladslopes@gmail.com>
2020-02-16 21:30:05 -0300

    Rename directories and files

---
+++ challenge01/README.md
@@ -0,0 +1,37 @@
+<h1 align="center">Challenge 1 - GoStack Bootcamp</h1>
+
+<p align="center">
+  <img alt="GitHub language count" src="https://img.shields.io/github/languages/count/gabrieladslopes/GoStack?color=%2304D361">
+  <img alt="GitHub top language" src="https://img.shields.io/github/languages/top/gabrieladslopes/GoStack">
+  <img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/gabrieladslopes/GoStack">
+  <img alt="License" src="https://img.shields.io/badge/license-MIT-%2304D361">
+</p>
+
+## About
+
+First challenge of the GoStack Bootcamp that consists of an application used to store projects and their tasks using [Express](https://expressjs.com/).
+
+## Instructions
+
+The instructions for this challenge can be found at [Instructions](https://github.com/gabrieladslopes/GoStack/blob/master/desafio01/ChallengeInstructions.md).
+
+## Setup
+
+* Install [NodeJS](https://nodejs.org/en/)
+* Install [Yarn](https://yarnpkg.com/)
+
+## How to use
+
+* Clone this repository:
+`git clone https://github.com/gabrieladslopes/GoStack.git`
+* Change to the challenge directory:
+`cd challenge01`
+* Install project dependencies:
+`yarn install`
+* Start the server:
+`node index.js`
+* Navigate to http://localhost:3333
+
+## Routes
+
+
---
+++ challenge01/index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const projects = [];
+
+// Middlewares
+function checkIfProjectExists (req, res, next) {
+  const { id } = req.params;
+  
+  if(!projects.find(project => project.id == id)) {
+    return res.status(400).json({ error: `Project with id ${id} not found` });
+  }
+
+  return next();
+}
+
+function logNumberOfRequests(req, res, next) {
+  console.count("Number of Requests");
+
+  return next();
+}
+
+app.use(logNumberOfRequests);
+
+// Routes
+app.post('/projects', (req, res) => {
+  const { id, title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  }
+
+  projects.push(project);
+
+  return res.json(project);
+});
+
+app.get('/projects', (req, res) => {
+  return res.json(projects);
+});
+
+app.put('/projects/:id', checkIfProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(project => project.id == id);
+
+  project.title = title;
+  return res.json(project);
+});
+
+app.delete('/projects/:id', checkIfProjectExists, (req, res) => {
+  const { id } = req.params;
+  const projectId = projects.findIndex(project => project.id == id);
+
+  projects.splice(projectId, 1);
+  return res.send();
+});
+
+app.post('/projects/:id/tasks', checkIfProjectExists, (req, res) => {
+  const { title } = req.body;
+  const { id } = req.params;
+
+  const project = projects.find(project => project.id == id);
+
+  project.tasks.push(title);
+  return res.json(project);
+});
+
+app.listen(3333);
